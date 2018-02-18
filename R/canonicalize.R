4ae76ab75c5087535b21307fe84b4ccc4e2b5505
Thomas Weise <tweise@home>
2018-02-18 18:20:29 +0800

    BugFix: Constant Functions Now Ignored
    Functions of the form `function(x) 5`
    would have led to errors before. Now we
    simply leave them as is.

--- R/canonicalize.R
+++ R/canonicalize.R
@@ -44,55 +44,58 @@ function.canonicalize <- function(f) {
   f <- base::force(f);
   # If the function is primitive, then it already is "canonical"
   if(base::is.function(f) && (!(base::is.primitive(f)))) {
-    # Get the environment of f
-    f.env <- base::environment(f);
-    f.env <- base::force(f.env);
-    if(base::is.null(f.env)) {
-      f.env <- .GlobalEnv;
-      f.env <- base::force(f.env);
-    }
-    f.env.ls <- base::ls(envir = f.env)
-    f.env.ls <- base::force(f.env.ls);
-
-    # We now try to resolve all promises in f by applying the solution given by
-    # schloerke at http://github.com/hadley/pryr/issues/43
     f.body <- base::body(f);
     f.body <- base::force(f.body);
-    f.body <- pryr::modify_lang(x=f.body,
-                                f=function(x) {
-                                  if (base::is.name(x)) {
-                                    x.deparsed <- base::deparse(x)
-                                    if (x.deparsed %in% f.env.ls) {
-                                      # OK, we can resolve the name x
-                                      return(base::get(x.deparsed, envir = f.env))
+    if(is.language(f.body)) {
+      # Get the environment of f
+      f.env <- base::environment(f);
+      f.env <- base::force(f.env);
+      if(base::is.null(f.env)) {
+        f.env <- .GlobalEnv;
+        f.env <- base::force(f.env);
+      }
+      f.env.ls <- base::ls(envir = f.env)
+      f.env.ls <- base::force(f.env.ls);
+
+      # We now try to resolve all promises in f by applying the solution given by
+      # schloerke at http://github.com/hadley/pryr/issues/43
+      f.body <- pryr::modify_lang(x=f.body,
+                                  f=function(x) {
+                                    if (base::is.name(x)) {
+                                      x.deparsed <- base::deparse(x)
+                                      if (x.deparsed %in% f.env.ls) {
+                                        # OK, we can resolve the name x
+                                        return(base::get(x.deparsed, envir = f.env))
+                                      }
                                     }
-                                  }
-                                  return(x);
-                                })
-    f.body <- base::force(f.body);
+                                    return(x);
+                                  })
+      f.body <- base::force(f.body);
 
-    # We now apply the second line of resolution, namely substitute_q
-    f.body <- pryr::substitute_q(x=f.body, env=f.env);
-    f.body <- base::force(f.body);
 
-    # Now with substitute direct to clean up
-    f.body <- methods::substituteDirect(object=f.body, frame=f.env, cleanFunction=TRUE);
-    f.body <- base::force(f.body);
+      # We now apply the second line of resolution, namely substitute_q
+      f.body <- pryr::substitute_q(x=f.body, env=f.env)
+      f.body <- base::force(f.body);
 
-    # Now we try to evaluate all evaluate-able sub-expressions of f.
-    f.body <- functionComposeR::expression.simplify(f.body, f.env);
-    f.body <- base::force(f.body);
+      # Now with substitute direct to clean up
+      f.body <- methods::substituteDirect(object=f.body, frame=f.env, cleanFunction=TRUE);
+      f.body <- base::force(f.body);
 
-    # After the body has been resolved as far is it is possible, we re-compose
-    # the function
-    f <- pryr::make_function(args=base::formals(f), body=f.body, env=f.env)
-    f <- base::force(f);
+      # Now we try to evaluate all evaluate-able sub-expressions of f.
+      f.body <- functionComposeR::expression.simplify(f.body, f.env);
+      f.body <- base::force(f.body);
 
-    # Now we apply the default unenclose method from pryr (for good measures)
-    f <- pryr::unenclose(f=f);
+      # After the body has been resolved as far is it is possible, we re-compose
+      # the function
+      f <- pryr::make_function(args=base::formals(f), body=f.body, env=f.env)
+      f <- base::force(f);
 
-    # Finally, we fix the environment
-    base::environment(f) <- f.env;
+      # Now we apply the default unenclose method from pryr (for good measures)
+      f <- pryr::unenclose(f=f);
+
+      # Finally, we fix the environment
+      base::environment(f) <- f.env;
+    }
   }
 
   f <- force(f);
